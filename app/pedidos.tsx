27b52e768636f136d9abe3285bccff49b62ce923
danielgm23 <danielgm.webcontato@gmail.com>
2024-11-26 14:59:13 -0300

    first commit

---
+++ app/pedidos.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const pedidos = () => {
+  return (
+    <View>
+      <Text>teste</Text>
+    </View>
+  );
+};
+
+export default pedidos;
